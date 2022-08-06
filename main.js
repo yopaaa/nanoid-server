e1c9ac540e2c91c97711bd883cf36efea6e84316
yopa <90709631+yopaaa@users.noreply.github.com>
2022-08-06 12:54:01 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,11 +2,14 @@
 import express from "express";
 import {nanoid} from 'nanoid';
 const app = express()
+const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0'
 
 app.get('/', function (req, res) {
   res.send(`<center><h1 style="font-size:80px;">${nanoid(25)}</h1></center>`)
 })
 
-app.listen(8000, function () {
+console.log(host);
+
+app.listen(8000,host,function () {
   console.log('app listen on http://localhost:8000')
 })
