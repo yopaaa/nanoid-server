8cf4da2768d0ff8589bd3c976acf14aa40fb2cdb
yopa <90709631+yopaaa@users.noreply.github.com>
2022-08-07 09:29:32 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import express from "express";
 import {nanoid} from 'nanoid';
 const app = express()
 const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0'
+const port = 80;
 
 app.get('/', function (req, res) {
   res.send(`<center><h1 style="font-size:80px;">${nanoid(25)}</h1></center>`)
@@ -10,6 +11,6 @@ app.get('/', function (req, res) {
 
 console.log(host);
 
-app.listen(8000,host,function () {
-  console.log(`app listen on http://${host}:8000`)
+app.listen(port,host,function () {
+  console.log(`app listen on http://${host}:${port}`)
 })
