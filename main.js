403ba5e110567822a2c0c6bff6dfcac21efba24a
yopa <90709631+yopaaa@users.noreply.github.com>
2022-08-07 09:42:20 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import express from "express";
 import {nanoid} from 'nanoid';
 const app = express()
 const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0'
-const port = 80;
+const port = 8000;
 
 app.get('/', function (req, res) {
   res.send(`<center><h1 style="font-size:80px;">${nanoid(25)}</h1></center>`)
